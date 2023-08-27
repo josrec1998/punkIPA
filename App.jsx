962d7c3294d677f3153cdc97cef0e6686d48d5d2
Jose <jrecalde_98@hotmail.com>
2023-08-27 17:04:10 +0100

    Initial components

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import {React, useState, useEffect} from 'react';
+import BeerInfo from "./components/BeerInfo/BeerInfo";
+
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
